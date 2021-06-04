69e0d0b822bb85b906a68b485b15e676520775b4
Jason Bowerman <jasbowgames@gmail.com>
2021-06-03 21:06:06 -0500

    working on getting gtkmm working

---
+++ TwitchGtkApp/MainWindow.cpp
@@ -0,0 +1,6 @@
+
+MainWindow::MainWindow()
+{
+  set_title("Basic Application");
+  set_default_size(200,200);
+}
---
+++ TwitchGtkApp/MainWindow.h
@@ -0,0 +1,15 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <gtkmm-3.0.h>
+
+
+class MainWindow : pubic Gtk::Window{
+
+public:
+  MainWindow();
+private:
+
+};
+
+#endif
