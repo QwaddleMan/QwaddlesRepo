2d2eb2df3cab97f3ffe9a5134064a50c3442a371
Jason Bowerman <jasbowgames@gmail.com>
2021-06-06 15:19:40 -0500

    was having segfault issue I removed -g flag on compile and it doesn't segfault anymore. no clue why

--- TwitchGtkApp/src/MainWindow.cpp
+++ TwitchGtkApp/src/MainWindow.cpp
@@ -1,7 +1,28 @@
 #include "MainWindow.h"
+#include <iostream>
 
 MainWindow::MainWindow()
+  : m_button1("My Button")
 {
   set_title("Basic Application");
-  set_default_size(200,200);
+  set_border_width(10);
+
+  add(m_box);
+
+  m_button1.signal_clicked().connect(sigc::bind<Glib::ustring>(
+        sigc::mem_fun(*this, &MainWindow::on_button_clicked), "button 1"));
+
+  m_box.pack_start(m_button1);
+  m_button1.show();
+  m_box.show();
+}
+
+MainWindow::~MainWindow()
+{
+  
+}
+
+void MainWindow::on_button_clicked(Glib::ustring data)
+{
+  std::cout<<"hello\n";
 }
--- TwitchGtkApp/src/MainWindow.h
+++ TwitchGtkApp/src/MainWindow.h
@@ -1,15 +1,23 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <gtkmm.h>
+#include <gtkmm/box.h>
+#include <gtkmm/button.h>
+#include <gtkmm/window.h>
 
 
-class MainWindow : public Gtk::Window{
 
+class MainWindow : public Gtk::Window
+{
 public:
   MainWindow();
+  virtual ~MainWindow();
 private:
+protected:
+  void on_button_clicked(Glib::ustring data);
 
+  Gtk::Box m_box;
+  Gtk::Button m_button1;
 };
 
 #endif
